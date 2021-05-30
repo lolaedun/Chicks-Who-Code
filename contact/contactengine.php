91be23fc14c7123051689c1d20c61311b4c2871a
Lola <mledun@hotmail.com>
2021-05-30 11:40:44 +0000

    initial commit of old files

---
+++ contact/contactengine.php
@@ -0,0 +1,37 @@
+<?php
+
+$EmailFrom = Trim(stripslashes($_POST['Email']));
+$EmailTo = "hello@chickswhocode.com"; // <- Please add your email
+$Subject = "Message from Chicks Who Code";
+$Title = Trim(stripslashes($_POST['Name'])); 
+$Message = Trim(stripslashes($_POST['Message'])); 
+
+
+
+// validation
+$validationOK=true;
+if (!$validationOK) {
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+  exit;
+}
+
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $Name;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $Message;
+$Body .= "\n";
+
+// send email 
+$success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
+
+// redirect to success page 
+if ($success){
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=contactthanks.php\">";
+}
+else{
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+}
+?>
---
+++ contact/contactthanks.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<!--[if IE 8]> 				 <html class="no-js lt-ie9" lang="en" > <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en" > <!--<![endif]-->
+
+<head>
+  <!-- Facebook Pixel Code -->
+
+<!-- Facebook Conversion Code for contact us form- Chicks Who Code -->
+<script>(function() {
+  var _fbq = window._fbq || (window._fbq = []);
+  if (!_fbq.loaded) {
+    var fbds = document.createElement('script');
+    fbds.async = true;
+    fbds.src = 'https://connect.facebook.net/en_US/fbds.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(fbds, s);
+    _fbq.loaded = true;
+  }
+})();
+window._fbq = window._fbq || [];
+window._fbq.push(['track', '6055630699795', {'value':'0.00','currency':'USD'}]);
+</script>
+<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?ev=6055630699795&amp;cd[value]=0.00&amp;cd[currency]=USD&amp;noscript=1" /></noscript>
+
+<!-- End Facebook Pixel Code -->
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Thank You</title>
+  <meta name="author" content="Maria-Lola Conrich">
+  <meta name="description" content="########">
+  <meta name="keywords" content="########" />
+  <link rel="stylesheet" href="../css/foundation.css">
+  <link rel="stylesheet" href="../css/style.css">
+  <link href='http://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	
+	<box>
+		<div class="row">
+			<div class="large-3 columns">
+				<div class="animated bounceInLeft">
+				<sidebar>
+					<a href="../index.htm">
+						<logo>
+							<img src="../img/cwc-logo-dark-pink copy.png" alt="logo" />
+						</logo>
+					</a>
+					<intro>
+						Welcome to Chicks Who Code. Website Design, Development and SEO. By women on a mission to change the world. In a good way ofcourse.
+					<line></line><!-- separation line -->
+					</intro>
+					<nav>
+						<a href="../index.htm">Home</a>
+						<a href="../about.htm">About Lola</a>
+						<a href="../studio.htm">The studio</a>
+						<a href="../portfolio.htm">Portfolio</a>
+						<a href="../page.htm">Pricing</a>
+						<a href="#">Blog</a>
+					<line></line><!-- separation line -->
+					<worksnav>
+					    <h1>Work with Me</h1>
+					    	<a href="../page.htm">DIY Lessons</a>
+						    <a href="../page.htm">Web Design</a>
+						    <a href="../page.htm">Web Development</a>
+						    <a href="../page.htm">SEO</a>
+						    <a href="../page.htm">Speaking</a>
+					</worksnav>
+				</sidebar>
+				</div><!-- animation -->
+			</div>
+			<div class="large-9 columns">
+				<div class="animated fadeIn">
+				<img src="../img/hello-lola.jpg" width="100%" height="250">
+				<about>
+					<h1>Hello, is it me you're looking for?</h1>
+					<p>Please let me know details of your requirements and I will get back to you as soon as possible. Look forward to speaking with you soon.</p>
+					<line></line>
+					<div id="ok">
+						Your message has been successfully sent. Thank you
+					</div>
+				</about>
+				</div>
+				
+			</div>
+		</div>
+	</box>
+	<footer>Site Created and Developed by Maria Lola Conrich @Chicks Who Code. All rights reserved.
+				</footer>
+
+  <script>
+  document.write('<script src=' +
+  ('__proto__' in {} ? '../js/vendor/zepto' : '../js/vendor/jquery') +
+  '.js><\/script>')
+  </script>
+  
+  <script src="../js/foundation.min.js"></script>
+  
+  <!--
+  <script src="../js/foundation/foundation.js"></script>
+  <script src="../js/foundation/foundation.alerts.js"></script>
+  <script src="../js/foundation/foundation.clearing.js"></script>
+  <script src="../js/foundation/foundation.orbit.js"></script>
+  -->
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+  <script>
+    $(document).foundation();
+  </script>
+  <script>
+    window.onload = function() {
+        setTimeout(function(){window.scrollTo(0, 1);}, 1);
+    }
+  </script>
+</body>
+</html>
